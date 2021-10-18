ba83e84f8e22d068f3905ee277ddb5db6f82ab5c
Cullin Flynn <cullinflynn@rocketmortgage.com>
2021-10-18 09:02:48 -0400

    learning OOP baby

---
+++ Week2ListOfObjects/Program.cs
@@ -0,0 +1,31 @@
+﻿using StudentDatabase;
+using System;
+using System.Collections.Generic;
+
+namespace Week2ListOfObjects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<StudentsObject> students = new List<StudentsObject>();
+            students.Add(new StudentsObject("Cullin", "Fowlerville", "Pad Thai", 28));
+            students.Add(new StudentsObject("Bill", "Boston", "Chicken Masala", 87));
+            students.Add(new StudentsObject("Dan", "Dansville", "Danmeal", ));
+            students.Add(new StudentsObject("George", "Grand Rapids", "Chicken feet", 43));
+
+            Console.WriteLine($"There are {students.Count} students in the list so far.");
+
+            //Here we shall pass objects 'students', 'favFoods', and 'hometowns' into one Master List. 
+            //string[] masterlist = { Object1, Object 2, Object3 };
+
+            Console.WriteLine($"Here's some info on the students in the list:");
+
+            for (int i = 0; i <= students.Count-1; i++)
+            {
+                Console.WriteLine($"{i+1}: {students[i].StudentNameProperty} is from {students[i].HometownProperty}. {students[i].StudentNameProperty} is {students[i].StudentAge} years old.");
+            }
+        }
+    }
+}
---
+++ Week2ListOfObjects/StudentsObject.cs
@@ -0,0 +1,24 @@
+﻿namespace StudentDatabase
+{
+    class StudentsObject
+    {
+        public string HometownProperty { get; set; }
+        public string FavFoodProperty { get; set; }
+        public string StudentNameProperty { get; set; }
+
+        public int StudentAge { get; set; }
+
+        //now for the constructor to give 
+        public StudentsObject(string StudentNameProperty, string HometownProperty, string FavFoodProperty, int StudentAge)
+        {
+            //Store properties in the object:
+            this.StudentNameProperty = StudentNameProperty;
+            this.HometownProperty = HometownProperty;
+            this.FavFoodProperty = FavFoodProperty;
+            this.StudentAge = StudentAge;
+
+        }
+
+    }
+
+}
